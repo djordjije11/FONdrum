511ef8b1435f4d2c7af9eb9cb59d406df4d95981
djordjije11 <djordjijeradovic24@gmail.com>
2024-01-14 12:55:08 +0100

    Fixed PagesCount bug

--- server/FONdrum/FONdrum.Domain/Shared/Paginations/PageInfo.cs
+++ server/FONdrum/FONdrum.Domain/Shared/Paginations/PageInfo.cs
@@ -6,12 +6,13 @@
         public int? PageSize { get; init; }
         public int? PageNumber { get; init; }
 
+        public long? PagesCount => PageSize.HasValue ? (TotalCount + PageSize.Value - 1) / PageSize.Value : null;
+        
         public PageInfo(long totalCount, int? pageSize, int? pageNumber)
         {
             if (totalCount < 0 || pageSize < 0 || pageNumber < 0)
-            {
-                throw new ArgumentException();
-            }
+                throw new ArgumentException($"Arguments {nameof(totalCount)}, {nameof(pageSize)}, {nameof(pageNumber)}" +
+                    $" must be greater than or equal to 0.");
 
             TotalCount = totalCount;
             PageSize = pageSize;
@@ -19,7 +20,5 @@
         }
 
         public static PageInfo Empty(int? pageSize = null, int? pageNumber = null) => new PageInfo(0, pageSize, pageNumber);
-
-        public long? PagesCount => PageSize.HasValue ? TotalCount / PageSize.Value : null;
     }
 }
